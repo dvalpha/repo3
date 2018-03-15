fa9c8cc3c4c1fec0f6820ae5834414f62a7a71d4
dvalpha <archivo.millan@gmail.com>
2018-03-15 17:14:14 +0100

    primer push

---
+++ test3g/src/main/java/test3g/test3g/App.java
@@ -0,0 +1,13 @@
+package test3g.test3g;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello mondo!" );
+    }
+}
