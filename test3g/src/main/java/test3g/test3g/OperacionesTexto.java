3388b229a60e3a31ecd0ab33038c9234f9b460ab
dvalpha <archivo.millan@gmail.com>
2018-03-15 21:10:43 +0100

    metodo comentado

--- test3g/src/main/java/test3g/test3g/OperacionesTexto.java
+++ test3g/src/main/java/test3g/test3g/OperacionesTexto.java
@@ -2,6 +2,11 @@ package test3g.test3g;
 
 public class OperacionesTexto {
 
+	/**
+	 * 
+	 * @param cadena
+	 * @return
+	 */
 	public int tornaLongitudTexto(String cadena){
 		return cadena.length();
 	}
