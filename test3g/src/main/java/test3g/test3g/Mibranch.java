b57d5f9d091ac78ab3e92a80f440b3d13d44fbc7
dvalpha <archivo.millan@gmail.com>
2018-03-15 20:32:27 +0100

    RAMA2

--- test3g/src/main/java/test3g/test3g/Mibranch.java
+++ test3g/src/main/java/test3g/test3g/Mibranch.java
@@ -3,7 +3,7 @@ package test3g.test3g;
 public class Mibranch {
 
 	public static void main(String[] args) {
-		System.out.println("Esta es mi rama");
+		System.out.println("Esta es mi rama 2");
 
 	}
 
