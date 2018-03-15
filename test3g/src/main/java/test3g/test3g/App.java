560850475c3283d2af3343ec6c2bbaafa1e2eb6e
dvalpha <archivo.millan@gmail.com>
2018-03-15 21:08:04 +0100

    rama oscar 2

--- test3g/src/main/java/test3g/test3g/App.java
+++ test3g/src/main/java/test3g/test3g/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello mondo lorondo ondo muy ondo!" );
+        //System.out.println( "Hello mondo lorondo ondo muy ondo!" );
     }
 }
